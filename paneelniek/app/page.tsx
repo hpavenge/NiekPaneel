84509925d962002f3f08bd79c06d6613ff00f6b7
Nick van der Raaf <nickraaf@outlook.com>
2024-09-11 13:28:04 +0200

    init

---
+++ paneelniek/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="grid gap-8  mx-8 mt-8">
+      <div>
+        <h1 className="text-4xl font-medium mb-2">Uitleg niekert</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum mauris enim, id suscipit lectus rutrum vel. Integer ac odio odio. Praesent non nulla vitae enim elementum blandit quis ac massa. Donec vitae risus consectetur sem fermentum iaculis. Morbi euismod nec ex eu fringilla. Sed urna neque, auctor molestie magna vel, eleifend elementum ante. Aenean lacinia, tellus ac commodo volutpat, sem arcu tincidunt nunc, eget dignissim nibh quam sit amet turpis. Phasellus augue neque, tristique non odio quis, lacinia malesuada quam.
+Nulla non mi sed dui sagittis accumsan. Nam suscipit arcu quis purus placerat, nec lacinia magna laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam nec sapien id metus interdum pretium. Vivamus sit amet congue mi, id volutpat magna. Donec eu blandit ante, ut dignissim leo. Curabitur consequat, ligula id porta maximus, eros ante pellentesque felis, eu accumsan justo nunc non lectus. Donec facilisis interdum nisi, at efficitur massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p >
+      </div >
+      <div className="flex">
+        <a target="_blank"  rel="noopener noreferrer" href="https://www.google.nl" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button1
+        </a>
+        <a href="" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto lg:ml-10">
+          Button2 
+        </a>
+      </div>
+
+    </section >
+  );
+}
